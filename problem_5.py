76a6fa2a08cdd90ecce3e25e11337d88062d2ccd
Mariam Joan <mariamjoan@Mariams-MBP-2.lan1>
2019-05-30 22:02:07 -0700

    problem 5 remove suffixtrie

--- problem_5.py
+++ problem_5.py
@@ -4,52 +4,32 @@ class TrieNode:
     def __init__(self):
         self.is_word = False
         self.children = defaultdict()
-        self.key = None
     
-    def add(self, char):
-        if self is None:
-            self.children[char] = TrieNode()
+    def insert(self, char):
+        self.children[char] = TrieNode()
+    
+    def suffixes(self, prefix=""):
+        current = self
+        word_list = [current]
+        print(word_list)
+        
+        for key, value in current.children.items():
+            print('cur.child.key: ', key, 'val:', value)
+            if current.is_word:
+                word_list.append(prefix[:])
+            else: 
+                word_list.append(key)
+                # self.suffixes(value)  
+                word_list.pop()
+                print('popped from traverse', word_list)
+
+        return word_list
+        print('word: ', word_list)
             
-        if self is not None:
-            self.children[char]
     
-    def suffixes(self, suffix=""):       
-        if self.root is None:
-            self.root = current
-        
-        if self.root is not None:
-            current = self.root
-              
-        for char in suffix:
-            if char not in current.children:
-                return False
-            else:
-                current = current[char]
-        
-        if current is not None and '$' in current:
-            return current
-                    
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-    
-    def build_suffixes(self, word):
-        word += '$' 
-        
-        if self.root is None:
-            self.root = current
-        
-        if self.root is not None:
-            current = self.root
-        
-        for i in range(len(word)):
-            for char in word[i:]:
-                if char not in current.children:
-                    print('char', char)
-                    current.children = defaultdict()
-                else:
-                    current = current[char]
-                    print('current', current)
             
     def insert(self, word):
         if self.root is None:
@@ -59,9 +39,11 @@ class Trie:
             current = self.root
             
             for char in word:
+                print('char', char)
                 if char not in current.children:
                     current.children[char] = TrieNode()
                 current = current.children[char]
+                print('current', current)
         
         current.is_word = True
         print('trie', current.children)
@@ -77,24 +59,19 @@ class Trie:
             print('char in prefix: ',char)
             for child in current.children:
                 print('child in curr children: ', child)
-                if child != char:
-                    continue
-                elif child == char:
-                    return True
-                else:
+                if not current.children[char]:
                     return False
+                else:
+                    node = current.children[char]
+                    
+            return node
 
 wordList = [
     "ant", "anthology", "antagonist", "antonym", 
     "fun", "function", "factory", 
     "trie", "trigger", "trigonometry", "tripod"
 ]
-                
-MySuffixTrie = Trie()
-for word in wordList:
-    MySuffixTrie.build_suffixes(word)
-    print('MySuffixTrie', MySuffixTrie)
-            
+
 MyTrie = Trie()
 for word in wordList:
     MyTrie.insert(word)
@@ -107,13 +84,15 @@ def f(prefix):
     print('prefix', prefix)
     if prefix != '':
         prefixNode = MyTrie.find(prefix)
-        print('prefixNode: ', prefixNode)
+        print('prefixNode: ', prefixNode, type(prefixNode))
         if prefixNode:
-            print('\n'.join(prefixNode.suffixes()))
+            # print('\n'.join(prefixNode.suffixes()))
+            print('FINAL!', prefixNode.suffixes())
         else:
             print(prefix + " not found")
     else:
         print('')
 interact(f,prefix='')
 
-# Cite: http://www.cs.jhu.edu/~langmea/resources/lecture_notes/tries_and_suffix_tries.pdf
+# Citations:
+# 1. http://www.cs.jhu.edu/~langmea/resources/lecture_notes/tries_and_suffix_tries.pdf
