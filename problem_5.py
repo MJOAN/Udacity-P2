aab8deeb3491c07037352a2a2e51f3175f863fc3
Mariam Joan <mariamjoan@Mariams-MBP-2.lan1>
2019-06-01 19:19:31 -0700

    problem 6 router

--- problem_5.py
+++ problem_5.py
@@ -2,35 +2,52 @@ from collections import defaultdict
 
 class TrieNode:
     def __init__(self):
-        self.is_word = False
+        self.is_word_finished = False
         self.children = defaultdict()
-    
+        self.word_list = []
+
+    def __str__(self, level = 1):        
+        trie = "\t" * level + repr(self.children) + "\n"
+        for child in self.children:
+            trie += child.__str__() 
+        return trie
+
+    def __repr__(self):
+        return repr(self.children)
+
     def insert(self, char):
         self.children[char] = TrieNode()
-        
+    
     def suffixes(self, prefix=""):
         current = self
-        word_list = []
-        temp = "" 
-        print('word_list', word_list)
-                
+        not_found = False
+        suffix = ""
+        
+        for char in current.children.keys():
+            if not current.children[char]:
+                not_found = True
+                break
+                 
+            suffix += char
+            current = current.children[char]
+        
+        self.suffixes_recursive(current, suffix)
+        
+        for letters in self.word_list:
+            print(letters[0:])
+        
+    def suffixes_recursive(self, current, suffix): 
         if current.is_word_finished:
-            word_list.append(prefix[:])
-        
-        for key, value in current.children.items():
-            print('cur.child key: ', key, 'val:', value)
-            print('curr_child_len', len(current.children.items()))
-            temp += key
-            word_list.append(temp)
-            # self.suffix_recursive(temp)
-            print('recursive worked!, word_list: ', word_list)
-        
-        return word_list
+            self.word_list.append(suffix)
+            
+        for k, v in current.children.items():
+            self.suffixes_recursive(v, suffix + k)
     
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-            
+        self.word_list = [] 
+    
     def insert(self, word):
         if self.root is None:
             self.root = current
@@ -39,16 +56,16 @@ class Trie:
             current = self.root
             
             for char in word:
-                print('char', char)
-                if char not in current.children:
+                # print('char', char)
+                if char not in current.children:                    
                     current.children[char] = TrieNode()
+                    # print('cur.child: ', current.children)
                 current = current.children[char]
-                print('current', current)
         
-        current.is_word = True
-        print('trie', current.children)
+            current.is_word_finished = True
+            print('trie', current.children)
     
-    def find(self, prefix): # aka key 
+    def find(self, prefix): 
         if self.root is None:
             self.root = current
             
@@ -56,15 +73,12 @@ class Trie:
             current = self.root
                         
         for char in prefix:
-            print('char in prefix: ',char)
-            for child in current.children:
-                print('child in curr children: ', child)
-                if not current.children[char]:
-                    return False
-                else:
-                    node = current.children[char]
-                    
-            return node
+            # print('char in prefix: ',char)
+            if char not in current.children:
+                return None    
+            current = current.children[char]
+            
+        return current
 
 wordList = [
     "ant", "anthology", "antagonist", "antonym", 
@@ -75,24 +89,71 @@ wordList = [
 MyTrie = Trie()
 for word in wordList:
     MyTrie.insert(word)
-    
+
 from ipywidgets import widgets
 from IPython.display import display
 from ipywidgets import interact
 
 def f(prefix):
-    print('prefix', prefix)
     if prefix != '':
         prefixNode = MyTrie.find(prefix)
-        print('prefixNode: ', prefixNode, type(prefixNode))
+        # print('prefixNode: ', prefixNode)
         if prefixNode:
-            # print('\n'.join(prefixNode.suffixes()))
-            print('FINAL!', prefixNode.suffixes())
+            print(prefixNode.suffixes())
         else:
             print(prefix + " not found")
     else:
+        print(str(MyTrie))
         print('')
 interact(f,prefix='')
 
+# Cite: http://www.cs.jhu.edu/~langmea/resources/lecture_notes/tries_and_suffix_tries.pdf
+
 # Citations:
 # 1. http://www.cs.jhu.edu/~langmea/resources/lecture_notes/tries_and_suffix_tries.pdf
+
+
+
+
+
+################ DRAFTED CODE ####################################
+        # current = self
+        # print('current: ', current)
+        
+        # word_list = []
+        # word = list(prefix[:-1])
+        # print('word:  prefix:-1 -->', word)
+
+        # stack = []
+        # stack.append(prefix)
+        
+        # while stack:
+        #     node = stack.pop()
+        #     print('node', node)
+            
+        #     if node is None:
+        #         word.pop()
+        #         continue
+
+        #     word.append(node)  
+        #     print('word: ', word)
+        #     if current.is_word_finished:
+        #         word_list.append(''.join(word))
+        #         print('word_list: ', word_list)
+        #     for char in current.children.keys(): # get remaining suffixes
+        #         value = current.children[char] 
+        #         stack.append(None)
+        #         # stack.append(char)
+        #         stack.append(char)
+        #         print('stack append children chars', char)
+                
+        # print('word_list', word_list)
+        # return word_list
+
+    #     def suffixes(prefix, node, word_list):
+    #        current = node
+    #        word_list = []
+    #         if current.is_word_finished:
+    #             word_list.append(prefix)
+    #         for char in current.children:
+    #             suffixes(prefix + char, current.children[char], word_list)
--- problem_7.py
+++ problem_7.py
@@ -1,116 +1,115 @@
-# HTTPRouter using a Trie
-# For this exercise we are going to implement an HTTPRouter like you 
-# would find in a typical web server using the Trie data structure we learned previously.
+from collections import defaultdict
 
-# There are many different implementations of HTTP Routers such as regular 
-# expressions or simple string matching, but the Trie is an excellent and 
-# very efficient data structure for this purpose.
-
-# The purpose of an HTTP Router is to take a URL path like 
-# "/", "/about", or "/blog/2019-01-15/my-awesome-blog-post" 
-# and figure out what content to return. In a dynamic web server, 
-# the content will often come from a block of code called a handler.
-
-# First we need to implement a slightly different Trie than the one we 
-# used for autocomplete. Instead of simple words the Trie will contain a 
-# part of the http path at each node, building from the root node /
-
-# In addition to a path though, we need to know which function will handle 
-# the http request. In a real router we would probably pass an instance of 
-# a class like Python's SimpleHTTPRequestHandler which would be responsible 
-# for handling requests to that path. For the sake of simplicity we will just 
-# use a string that we can print out to ensure we got the right handler
-
-# We could split the path into letters similar to how we did the autocomplete
-# Trie, but this would result in a Trie with a very large number of nodes and 
-# lengthy traversals if we have a lot of pages on our site. A more sensible way 
-# to split things would be on the parts of the path that are separated by 
-# slashes ("/"). A Trie with a single path entry of: "/about/me" would look like:
-
-# (root, None) -> ("about", None) -> ("me", "About Me handler")
-
-# We can also simplify our RouteTrie a bit by excluding the suffixes method and 
-# the endOfWord property on RouteTrieNodes. We really just need to insert and 
-# find nodes, and if a RouteTrieNode is not a leaf node, it won't have a handler
-#  which is fine.
-
-# A RouteTrie will store our routes and their associated handlers
-class RouteTrie:
-    def __init__(self):
-        # Initialize the trie with an root node and a handler, this is the root 
-        # path or home page node
-        pass
-
-    def insert(self):
-        # Similar to our previous example you will want to recursively add nodes
-        # Make sure you assign the handler to only the leaf (deepest) node of this path
-        pass
-
-    def find(self):
-        # Starting at the root, navigate the Trie to find a match for this path
-        # Return the handler for a match, or None for no match
-        pass
-
-# A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
 class RouteTrieNode:
     def __init__(self):
-        # Initialize the node with children as before, plus a handler
-        pass
-    def insert(self):
-        # Insert the node as before
-        pass 
+        self.children = defaultdict()
+        self.handler = "home"   
+            
+    def get_node(self):   
+        return RouteTrieNode()     
+    
+    def insert(self, path):
+        self.children[path] = RouteTrieNode()
 
-
-# Next we need to implement the actual Router. The router will initialize 
-# itself with a RouteTrie for holding routes and associated handlers. 
-# It should also support adding a handler by path and looking up a handler 
-# by path. All of these operations will be delegated to the RouteTrie.
-
-# Hint: the RouteTrie stores handlers under path parts, so remember to split 
-# your path around the '/' character
-
-# Bonus Points: Add a not found handler to your Router which is returned
-#  whenever a path is not found in the Trie.
-
-# More Bonus Points: Handle trailing slashes! 
-# A request for '/about' or '/about/' are probably looking for the same page. 
-# Requests for '' or '/' are probably looking for the root handler. Handle these 
-# edge cases in your Router.
-
-# The Router class will wrap the Trie and handle 
-class Router:
+class RouteTrie:
     def __init__(self):
-        # Create a new RouteTrie for holding our routes
-        # You could also add a handler for 404 page not found responses as well!
-        pass 
-    def add_handler(self):
-        # Add a handler for a path
-        # You will need to split the path and pass the pass parts
-        # as a list to the RouteTrie
-        pass 
-    def lookup(self):
-        # lookup path (by parts) and return the associated handler
-        # you can return None if it's not found or
-        # return the "not found" handler if you added one
-        # bonus points if a path works with and without a trailing slash
-        # e.g. /about and /about/ both return the /about handler
-        pass 
+        self.root = RouteTrieNode()
+        self.handler = "/" # root
 
-    def split_path(self):
-        # you need to split the path into parts for 
-        # both the add_handler and loopup functions,
-        # so it should be placed in a function here
-        pass 
+    def __repr__(self):
+        return repr(self)
+    
+    def insert(self, path):
+        if self.root is None:
+            self.root = node
+        
+        if self.root is not None:
+            current = self.root
+    
+        for item in path:
+            print('item', item)
+            if item not in current.children:
+                print('item', item)
+                current.children[item] = RouteTrieNode()
+            
+            current = current.children[item]
+            print('curr', current)
+        
+        print('path: ', path)
+        current.handler = path # set final node to full handler ""
+            
+    def find(self, path):
+        if self.root is None:
+            self.root = current
+
+        if self.root is not None:
+            current = self.root
+        
+        for char in path:
+            print('char in prefix: ',char)
+            if char not in current.children:
+                return None
+            if current.children is None: # we are on the deepest leaf
+                print('handler: ', handler)
+                handler = current.handler    
+        
+        print('handler', handler)
+        return handler
+    
+class Router:
+    def __init__(self, path):
+        self.root = RouteTrie() # for holding routes
+    
+    def get_node(self):
+        return RouteTrieNode()
+    
+    def add_handler(self, path, handler):        
+        parts = self.split_path(path)
+        print('parst', parts)
+        handler_parts = ""
+        
+        for part in parts:
+            print('parts', part)
+            handler_parts += " " + part 
+            print('handler_parts: ', handler_parts)
+        
+        return handler_parts[0:]
+                    
+    def lookup(self, path):
+        current = self.get_node()
+        
+        parts = self.split_path(path)
+        print('parts', parts)
+        
+        for part in parts:
+            if part not in current.children.keys():
+                return None
+            current = current.children[part]
+            
+        return current
+
+    def split_path(self, path):
+        return path.split("/")        
+
+url_router = RouteTrie()
+print(url_router.insert("/home/about"))
+print(url_router.insert("/home/contact"))
+print(url_router.insert("/home/applications"))
+print(url_router.insert("/home/services"))
+
+router = Router("root handler")
+print('router', router)
+
+router.add_handler("/home/about", "about handler")  
+router.add_handler("/home/contact", "resume handler")  
+router.add_handler("/home/applications", "projects handler")  
+router.add_handler("/home/services", "contact handler") 
+
+router.add_handler("/home/about/resume", "about resume handler") 
+router.add_handler("/home/applications/python", "applications python handler") 
+router.add_handler("/home/services/pricing", "services pricing handler") 
 
 
-# Test Cases
-# Here are some test cases and expected outputs you can use to test your implementation
-
-# create the router and add a route
-router = Router("root handler", "not found handler") # remove the 'not found handler' if you did not implement this
-router.add_handler("/home/about", "about handler")  # add a route
-
-# some lookups with the expected output
 print(router.lookup("/")) # should print 'root handler'
 print(router.lookup("/home")) # should print 'not found handler' or None if you did not implement one
 print(router.lookup("/home/about")) # should print 'about handler'
